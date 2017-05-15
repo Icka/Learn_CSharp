39f766759c49d18972775aeceb374212ba46111b
Magdalena Janicka <magdalenajanicka93@gmail.com>
2017-05-15 22:23:31 +0200

    Init

---
+++ Read_File_line_by_line/Read_File_line_by_line/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+namespace Read_File_line_by_line
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines(@"C:\Users\magda\Desktop\ListOfPersons.txt");
+            foreach (var line in lines)
+            {
+                Console.WriteLine(" {0} ", String.IsNullOrEmpty(line) ? "<>" : line);
+            }
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}

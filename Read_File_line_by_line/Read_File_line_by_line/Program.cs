d0eb42efc88507af93d68890a650e6a6c882cc71
Magdalena Janicka <magdalenajanicka93@gmail.com>
2017-05-16 14:42:09 +0200

    New

--- Read_File_line_by_line/Read_File_line_by_line/Program.cs
+++ Read_File_line_by_line/Read_File_line_by_line/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 namespace Read_File_line_by_line
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var lines = File.ReadLines(@"C:\Users\magda\Desktop\ListOfPersons.txt");
+            Console.OutputEncoding = System.Text.Encoding.ASCII;
+            var lines = File.ReadLines(@"C:\Users\magda\Desktop\ListOfPersons.txt", Encoding.ASCII);
             foreach (var line in lines)
             {
                 Console.WriteLine(" {0} ", String.IsNullOrEmpty(line) ? "<>" : line);
